c36e2c63fccc5238588c180f8fae9ca4eaca9c67
Precia <wprecia4@gmail.com>
2018-10-22 09:36:28 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
+
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import * as firebase from 'firebase';
+const config = {
+  apiKey: "AIzaSyABTii28GziUzot61Lyj8SoJsd85-fCi9w",
+  authDomain: "my-first-firebase-projec-5f38c.firebaseapp.com",
+  databaseURL: "https://my-first-firebase-projec-5f38c.firebaseio.com",
+  projectId: "my-first-firebase-projec-5f38c",
+  storageBucket: "my-first-firebase-projec-5f38c.appspot.com",
+  messagingSenderId: "375056967621"
+}; 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage:any 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +26,16 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
     });
+    firebase.initializeApp(config);
+    const unsubscribe = firebase.auth().onAuthStateChanged(user=>{
+      if(!user){
+        this,this.rootPage='LoginPage'
+        unsubscribe();
+      }else{
+        this.rootPage='RoomsPage'
+        unsubscribe();
+      }
+    })
   }
 }
 
